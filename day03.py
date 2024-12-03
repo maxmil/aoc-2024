c2b726b972bf012c555fc6593d1247e824bbbe35
Max Pimm <max.pimm@triptease.com>
2024-12-03 08:29:05 +0100

    Day 2 - Cleaned up

--- day03.py
+++ day03.py
@@ -1,10 +1,6 @@
 import re
 from functools import reduce
 
-
-def parse_input(filename):
-    return [[int(x) for x in l.split()] for l in open(filename).read().splitlines()]
-
 def mult(s):
     matches = re.finditer(r'mul\((\d+),(\d+)\)', s)
     return sum(reduce(lambda x, y: int(x) * int(y), match.groups()) for match in matches)
@@ -13,39 +9,21 @@ def part1(filename):
     return mult(open(filename).read())
 
 def part2(filename):
-    data = open(filename).read()
-    ans =0
+    mem = open(filename).read()
+    ans = 0
     enabled = True
-    while len(data) > 0:
+    while len(mem) > 0:
         if enabled:
-            i = data.find("don't()")
-            if i == -1:
-                i = len(data)
-            else:
-                i += len("don't()")
-            ans += mult(data[:i])
-            data = data[i:]
-            enabled = False
+            i = len(mem) if (i := mem.find("don't()")) == -1 else i + len("don't()")
+            ans += mult(mem[:i])
         else:
-            i = data.find("do()")
-            if i == -1:
-                i = len(data)
-            else:
-                i += len("do()")
-            data = data[i:]
-            enabled = True
-        print(data)
-        print(ans)
-    print(ans)
+            i = len(mem) if (i := mem.find("do()")) == -1 else i + len("do()")
+        mem = mem[i:]
+        enabled = not enabled
     return ans
 
-# def part2(filename):
-#     return sum(any(is_safe(record[:i] + record[i + 1:]) for i in range(len(record))) for record in parse_input(filename))
-
-# assert part1('day03_input_test_1.txt') == 161
-# assert part1('day03_input.txt') == 173731097
-# print(f'Part 1: {part1('day03_input.txt')}')
-#
+assert part1('day03_input_test_1.txt') == 161
+print(f'Part 1: {part1('day03_input.txt')}')
 
 assert part2('day03_input_test_2.txt') == 48
 print(f'Part 2: {part2('day03_input.txt')}')
