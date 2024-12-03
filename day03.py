92dfe17ef833aab4095786394589e4e60dcd942f
Max Pimm <max.pimm@triptease.com>
2024-12-03 09:04:23 +0100

    Day 2 - Function name should match the text

--- day03.py
+++ day03.py
@@ -1,10 +1,10 @@
 import re
 
-def mult(s):
+def mul(s):
     return sum(int(match[0]) * int(match[1]) for match in (re.findall(r'mul\((\d+),(\d+)\)', s)))
 
 def part1(filename):
-    return mult(open(filename).read())
+    return mul(open(filename).read())
 
 def part2(filename):
     mem = open(filename).read()
@@ -13,7 +13,7 @@ def part2(filename):
     states = ['do()', "don't()"]
     while len(mem) > 0:
         i = len(mem) if (i := mem.find(states[state])) == -1 else i + len(states[state])
-        if state: ans += mult(mem[:i])
+        if state: ans += mul(mem[:i])
         state = (state + 1) % 2
         mem = mem[i:]
     return ans
