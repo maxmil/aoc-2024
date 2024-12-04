1865168e64aac845554965567aa839aa9f1c5e85
Max Pimm <maxmil@gmail.com>
2024-12-04 16:25:08 +0100

    Day 4 - part 2

--- day04.py
+++ day04.py
@@ -1,6 +1,7 @@
 def transpose(grid):
     return [[grid[y][x] for y in range(len(grid[x]))] for x in range(len(grid))]
 
+
 def diagonal(grid, right):
     h = len(grid)
     w = len(grid[0])
@@ -13,18 +14,32 @@ def diagonal(grid, right):
                 d[x + y].append(grid[y][x])
     return d
 
+
 def count(grid):
     return sum(''.join(row).count(s) for s in ['XMAS', 'SAMX'] for row in grid)
+
 
 def part1(filename):
     grid = open(filename).read().splitlines()
     return sum(count(g) for g in [grid, transpose(grid), diagonal(grid, False), diagonal(grid, True)])
 
+
 def part2(filename):
-    return 0
+    grid = open(filename).read().splitlines()
+    h = len(grid)
+    w = len(grid[0])
+    ans = 0
+    for y, row in enumerate(grid):
+        for x, c in enumerate(row):
+            if c == 'A' and 0 < x < w - 1 and 0 < y < h - 1:
+                if {grid[y - 1][x - 1], grid[y + 1][x + 1]} == {'S', 'M'}:
+                    if {grid[y + 1][x - 1], grid[y - 1][x + 1]} == {'S', 'M'}:
+                        ans += 1
+    return ans
+
 
 assert part1('day04_input_test.txt') == 18
 print(f'Part 1: {part1('day04_input.txt')}')
 
-# assert part2('day04_input_test.txt') == 9
-# print(f'Part 2: {part2('day04_input.txt')}')
+assert part2('day04_input_test.txt') == 9
+print(f'Part 2: {part2('day04_input.txt')}')
