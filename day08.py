b64c897200bfb9eb417439b2e45ac07d573cd418
Max Pimm <max.pimm@triptease.com>
2024-12-08 11:25:36 +0100

    Day 8 - Cleaned up

--- day08.py
+++ day08.py
@@ -1,49 +1,35 @@
 def in_bounds(p, grid):
-    return 0 <=  p.real < len(grid[0]) and 0 <= p.imag < len(grid)
+    return 0 <= p.real < len(grid[0]) and 0 <= p.imag < len(grid)
 
-def part1(filename):
-    grid = open(filename).read().split()
+
+def find_antinodes(grid, max_dist=float('inf'), exclude_source=False):
     antennas = {}
-    resonance = set()
+    antinodes = set()
     for y, row in enumerate(grid):
         for x, freq in enumerate(row):
-          if freq != '.':
-              p1 = x + y * 1j
-              antennas_freq = antennas.setdefault(freq, [])
-              for p2 in antennas_freq:
-                r1 = 2 * p2 - p1
-                r2 = 2 * p1 - p2
-                if in_bounds(r1, grid):
-                    resonance.add(r1)
-                if in_bounds(r2, grid):
-                    resonance.add(r2)
-              antennas_freq.append(p1)
-    ans = len(resonance)
-    return ans
+            if freq != '.':
+                antenna = x + y * 1j
+                freq = antennas.setdefault(freq, [])
+                for other in freq:
+                    wavelength = other - antenna
+                    i = 1 if exclude_source else 0
+                    while i <= max_dist:
+                        nodes = [n for n in [antenna - wavelength * i, other + wavelength * i] if in_bounds(n, grid)]
+                        if len(nodes):
+                            antinodes.update(nodes)
+                            i += 1
+                        else:
+                            break
+                freq.append(antenna)
+    return antinodes
+
+
+def part1(filename):
+    return len(find_antinodes(open(filename).read().split(), 1, True))
 
 
 def part2(filename):
-    grid = open(filename).read().split()
-    antennas = {}
-    resonance = set()
-    for y, row in enumerate(grid):
-        for x, freq in enumerate(row):
-          if freq != '.' and freq  != '#':
-              p1 = x + y * 1j
-              antennas_freq = antennas.setdefault(freq, [])
-              for p2 in antennas_freq:
-                  dist = p2 - p1
-                  i = 0
-                  while in_bounds(p2 + i * dist, grid):
-                      resonance.add(p2 + i * dist)
-                      i += 1
-                  i = 0
-                  while in_bounds(p1 - i * dist, grid):
-                      resonance.add(p1 - i * dist)
-                      i += 1
-              antennas_freq.append(p1)
-    ans = len(resonance)
-    return ans
+    return len(find_antinodes(open(filename).read().split()))
 
 
 assert part1('day08_input_test.txt') == 14
