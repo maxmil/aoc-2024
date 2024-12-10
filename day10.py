16c5cfac24decc3dd044b4a40875d2d1ad50fabc
Max Pimm <max.pimm@triptease.com>
2024-12-10 07:21:08 +0100

    Day 10 - Cleaned up

--- day10.py
+++ day10.py
@@ -1,11 +1,8 @@
-dirs = [1, -1j, -1, 1j]
-
-
 def trails(filename):
     grid = {i + j * 1j: int(c) for i, r in enumerate(open(filename)) for j, c in enumerate(r.strip())}
     paths = [[p] for p in grid if grid[p] == 0]
     for i in range(1, 10):
-        paths = [p + [p[-1] + d] for d in dirs for p in paths if p[-1] + d in grid and grid[p[-1] + d] == i]
+        paths = [p + [n] for d in [1, -1j, -1, 1j] for p in paths if ((n := p[-1] + d) in grid and grid[n] == i)]
     return paths
 
 
@@ -18,7 +15,6 @@ def part2(filename):
 
 
 assert part1('day10_input_test.txt') == 36
-assert part1('day10_input.txt') == 582
 print(f'Part 1: {part1('day10_input.txt')}')
 
 assert part2('day10_input_test.txt') == 81
