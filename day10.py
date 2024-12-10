6f110c51a7ec6d62dd7f0897d21bf3947b6184c2
Max Pimm <max.pimm@triptease.com>
2024-12-10 07:09:08 +0100

    Day 10 - Part 2

--- day10.py
+++ day10.py
@@ -1,20 +1,25 @@
-
 dirs = [1, -1j, -1, 1j]
 
-def part1(filename):
+
+def trails(filename):
     grid = {i + j * 1j: int(c) for i, r in enumerate(open(filename)) for j, c in enumerate(r.strip())}
     paths = [[p] for p in grid if grid[p] == 0]
     for i in range(1, 10):
         paths = [p + [p[-1] + d] for d in dirs for p in paths if p[-1] + d in grid and grid[p[-1] + d] == i]
-    ans = len(set((p[0], p[-1]) for p in paths))
-    return ans
+    return paths
+
+
+def part1(filename):
+    return len(set((p[0], p[-1]) for p in (trails(filename))))
+
 
 def part2(filename):
-    return 0
+    return len((trails(filename)))
+
 
 assert part1('day10_input_test.txt') == 36
 assert part1('day10_input.txt') == 582
 print(f'Part 1: {part1('day10_input.txt')}')
 
-assert part2('day10_input_test.txt') == 2858
+assert part2('day10_input_test.txt') == 81
 print(f'Part 2: {part2('day10_input.txt')}')
