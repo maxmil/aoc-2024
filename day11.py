ce7b81dfe92a83a55731e05d0a99979a3ff65d44
Max Pimm <maxmil@gmail.com>
2024-12-11 22:10:03 +0100

    Day 7 - User itertools.cache

--- day11.py
+++ day11.py
@@ -1,29 +1,20 @@
-def count_stones(stone, blinks, max_blinks, cache):
-    if blinks == max_blinks:
-        return 1
-    if (stone, blinks) in cache:
-        return cache[(stone, blinks)]
-    cache[(stone, blinks)] = (
-        count_stones(1, blinks + 1, max_blinks, cache) if stone == 0 else
-        sum(count_stones(s, blinks + 1, max_blinks, cache) for s in map(int, (str(stone)[:l // 2], str(stone)[l // 2:]))) if (l := len(str(stone))) % 2 == 0 else
-        count_stones(stone * 2024, blinks + 1, max_blinks, cache)
-    )
-    return cache[(stone, blinks)]
+from functools import cache
+
+
+@cache
+def stones(stone, blinks):
+    if blinks == 0: return 1
+    if stone == 0: return stones(1, blinks - 1)
+    s = str(stone)
+    if len(s) % 2 == 0:
+        return sum(stones(s, blinks - 1) for s in map(int, (s[:len(s) // 2], s[len(s) // 2:])))
+    return stones(stone * 2024, blinks - 1)
 
 
 def solve(filename, blinks):
-    return sum(count_stones(s, 0, blinks, {}) for s in list(map(int, open(filename).read().split())))
+    return sum(stones(s, blinks) for s in map(int, open(filename).read().split()))
 
 
-def part1(filename):
-    return solve(filename, 25)
-
-
-def part2(filename):
-    return solve(filename, 75)
-
-
-assert part1('day11_input_test.txt') == 55312
-print(f'Part 1: {part1('day11_input.txt')}')
-
-print(f'Part 2: {part2('day11_input.txt')}')
+assert solve('day11_input_test.txt', 25) == 55312
+print(f'Part 1: {solve('day11_input.txt', 25)}')
+print(f'Part 2: {solve('day11_input.txt', 75)}')
