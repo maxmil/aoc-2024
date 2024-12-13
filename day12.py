bc5e58b2a598be3306c23f4d655335430077f58b
Max Pimm <max.pimm@triptease.com>
2024-12-13 10:12:51 +0100

    Day 12 - Cleaned up

--- day12.py
+++ day12.py
@@ -1,35 +1,16 @@
-def plant_at(p, grid):
-    return grid[p] if p in grid else None
-
-
 def get_corners(point, grid, plant):
-    # if plant == 'C': print(plant, point)
     corners = set()
     diagonals = [1 - 1j, 1 + 1j, -1 - 1j, -1 + 1j]
-
-    # inner corners
-    for diagonal in diagonals:
-        if plant_at(point + diagonal, grid) != plant and plant_at(point + diagonal.real, grid) == plant and plant_at(point + diagonal.imag * 1j, grid) == plant:
-            # if plant == 'C': print(f"inner {point} {diagonal}")
-            corners.add(point + diagonal)
-
-    # outer corners
-    for diagonal in diagonals:
-        if plant_at(point + diagonal.real, grid) != plant and plant_at(point + diagonal.imag * 1j, grid) != plant:
-            # if plant == 'C': print(f"outer {point} {diagonal}")
-            corners.add(point + diagonal)
-
-    # if plant == 'C': print(corners)
+    for d in diagonals:
+        if grid.get(point + d) != plant and grid.get(point + d.real) == plant and grid.get(point + d.imag * 1j) == plant:
+            corners.add(point + d)
+        if grid.get(point + d.real) != plant and grid.get(point + d.imag * 1j) != plant:
+            corners.add(point + d)
     return list(corners)
 
 
 def discover_region(point, grid, seen):
-    plant = grid[point]
-    seen.add(point)
-    region = {point}
-    perimeter = []
-    corners = get_corners(point, grid, plant)
-    to_visit = [point + d for d in [1, 1j, -1, -1j]]
+    plant, region, perimeter, corners, to_visit = grid[point], set(), [], [], [point]
     while to_visit:
         p = to_visit.pop()
         if p in grid and grid[p] == plant:
@@ -40,15 +21,12 @@ def discover_region(point, grid, seen):
                 to_visit = to_visit + [p + d for d in [1, 1j, -1, -1j]]
         else:
             perimeter.append(p)
-    # print(corners)
-    # exit()
     return region, perimeter, corners
 
 
 def find_regions(filename):
     grid = {x + y * 1j: c for y, r in enumerate(open(filename)) for x, c in enumerate(r.strip())}
-    regions = []
-    seen = set()
+    regions, seen = [], set()
     for p in grid:
         if not p in seen:
             regions.append(discover_region(p, grid, seen))
@@ -56,24 +34,15 @@ def find_regions(filename):
 
 
 def part1(filename):
-    regions = find_regions(filename)
-    return sum(len(plants) * len(perimeter) for (plants, perimeter, _) in regions)
+    return sum(len(plants) * len(perimeter) for (plants, perimeter, _) in find_regions(filename))
 
 
 def part2(filename):
-    grid = {x + y * 1j: c for y, r in enumerate(open(filename)) for x, c in enumerate(r.strip())}
-    regions = find_regions(filename)
-    for plants, _, corners in regions:
-        print(grid[list(plants)[0]], len(plants), len(corners))
-    ans = sum(len(plants) * len(corners) for (plants, _, corners) in regions)
-    print(ans)
-    return ans
+    return sum(len(plants) * len(corners) for (plants, _, corners) in find_regions(filename))
 
 
-# assert part1('day12_input_test.txt') == 1930
-# print(f'Part 1: {part1('day12_input.txt')}')
-# 1437300
+assert part1('day12_input_test.txt') == 1930
+print(f'Part 1: {part1('day12_input.txt')}')
 
 assert part2('day12_input_test.txt') == 1206
 print(f'Part 2: {part2('day12_input.txt')}')
-# 849332
