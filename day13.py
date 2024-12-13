6b78ee4acf63faece1eedf874d392b1e0a900cff
Max Pimm <max.pimm@triptease.com>
2024-12-13 08:01:34 +0100

    Day 13 - Cleaned up

--- day13.py
+++ day13.py
@@ -2,32 +2,33 @@ import re
 
 
 def parse(filename):
-    blocks = [c.split('\n') for c in (open(filename).read().split('\n\n'))]
+    claws = [c.split('\n') for c in (open(filename).read().split('\n\n'))]
     return [(
         (int(a[12:14]), int(a[18:20])),
         (int(b[12:14]), int(b[18:20])),
         (int(re.split(r'X=|,', prizes)[1]), int(prizes.split('Y=')[1]))
-    ) for a, b, prizes in blocks]
+    ) for a, b, prizes in claws]
 
 
-def resolve(claw):
+def tokens(claw):
     a = ((claw[1][0] * claw[2][1]) - (claw[1][1] * claw[2][0])) / ((claw[1][0] * claw[0][1]) - (claw[1][1] * claw[0][0]))
     b = ((claw[0][0] * claw[2][1]) - (claw[0][1] * claw[2][0])) / ((claw[0][0] * claw[1][1]) - (claw[1][0] * claw[0][1]))
     if a.is_integer() and b.is_integer(): return int(a) * 3 + int(b)
+    else: return 0
 
 
 def part1(filename):
-    claws = parse(filename)
-    return sum(tokens for claw in claws if (tokens := resolve(claw)) is not None)
+    return sum(tokens(claw) for claw in (parse(filename)))
 
 
 def part2(filename):
-    claws = parse(filename)
-    big_claws = [(claw[0], claw[1], tuple(map(lambda n: 10000000000000 + n, claw[2]))) for claw in claws]
-    return sum(tokens for claw in big_claws if (tokens := resolve(claw)) is not None)
+    claws = [(*claw[:2], [n + 10 ** 13 for n in claw[2]]) for claw in (parse(filename))]
+    return sum(tokens(claw) for claw in claws)
 
 
 assert part1('day13_input_test.txt') == 480
+assert part1('day13_input.txt') == 26810
 print(f'Part 1: {part1('day13_input.txt')}')
 
+assert part2('day13_input.txt') == 108713182988244
 print(f'Part 2: {part2('day13_input.txt')}')
