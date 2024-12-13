317ece4473d7817f479bc5d99c0822085834aeee
Max Pimm <max.pimm@triptease.com>
2024-12-13 07:54:39 +0100

    Day 13 - Part 2

--- day13.py
+++ day13.py
@@ -22,11 +22,12 @@ def part1(filename):
 
 
 def part2(filename):
-    return 0
+    claws = parse(filename)
+    big_claws = [(claw[0], claw[1], tuple(map(lambda n: 10000000000000 + n, claw[2]))) for claw in claws]
+    return sum(tokens for claw in big_claws if (tokens := resolve(claw)) is not None)
 
 
 assert part1('day13_input_test.txt') == 480
 print(f'Part 1: {part1('day13_input.txt')}')
 
-assert part2('day13_input_test.txt') == -1
 print(f'Part 2: {part2('day13_input.txt')}')
