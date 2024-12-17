f45ec34b7ea94de0d11619b4c363dcd131fa3bd8
Max Pimm <maxmil@gmail.com>
2024-12-17 09:52:07 +0100

    Day 17 - OMG I can't believe i managed it
    Whaaat. That was crazy. I feel very proud of myself for managing to work it out. Well done Brian!
    
    First I did a few things that were fun but didn't really help.
    
    I wrote out my program in code and realised it was essentially a while loop that decreased a by a factor of 8 in each iteration till it reached zero, adding one output each time.
    
    So for our 16 character output it had to be a number between 8^15 and 8^16. Unfortunately that still left a big search space.
    
    Simplifying the program once I'd written it down got me to this simpler form having eliminated b and c.
    
        while a != 0:
            out.append(((a % 8) ^ (a // 2 ** ((a % 8) ^ 1)) % 8) ^ 4)
            a = a // 8
    
    Then I think I took the same approach as you Brian. I took the last output (16th) and inferred the previous one based on
    a // 8 had to be zero, so a could be from 0 to 7
    Test those values individually using the expression from above
    ((a % 8) ^ (a // 2 ** ((a % 8) ^ 1)) % 8) ^ 4 = n
    Where n was the value of the last output. This gave me possible value/s of a 1 output before the program completed.
    
    Then I looked at the next digit left, the 15th.
    a // 8 has to one of the values I calculated above z, so it had to be from  z * 8 to z * 8 + 7
    Again I tested those 8 values individually to get all the possible values of a 2 outputs before the program completed
    
    I then had to make this a dfs since there were digits that had multiple solutions but some of them resulted in problem that had no solution further down the line.
    
    I also had to take care to make sure my dfs searched for solutions in increasing order since the problem statement wanted the smallest a which satisfied the condition.
    
    And when it worked..... I couldn't believe it.
    
    I don't intend to refactor anything here. I am quite proud of the complete chaos that was my brain wrestling to find the right number and not thinking about coding at all.

--- day17.py
+++ day17.py
@@ -2,7 +2,7 @@ def combo(operand, registers):
     return operand if operand < 4 or operand == 7 else registers[operand - 4]
 
 
-def part1(registers, program):
+def run(program, registers):
     out = []
     pointer = 0
     while pointer < len(program):
@@ -26,39 +26,126 @@ def part1(registers, program):
         if opcode == 7:
             registers[2] = registers[0] // 2 ** combo(operand, registers)
         pointer += 2
+        if program[0:len(out)] != out:
+            return out
         # print(registers)
-
-    ans = ','.join(map(str, out))
-    print(ans)
-    return ans
+    return out
 
 
-def part2(registers, program):
-    return ""
+def part1(registers, program):
+    return ','.join(map(str, run(program, registers)))
 
 
-test_cases = [
-    ([729, 0, 0], [0, 1, 5, 4, 3, 0], '4,6,3,5,6,3,5,2,1,0', None),
-    ([10, 0, 0], [5, 0, 5, 1, 5, 4], '0,1,2', None),
-    ([2024, 0, 0], [0, 1, 5, 4, 3, 0], '4,2,5,6,7,7,7,7,3,1,0', [0, 0, 0]),
-    ([0, 29, 0], [1, 7], None, [0, 26, 0]),
-    ([0, 2024, 43690], [4, 0], None, [0, 44354, 43690])
-]
+def part2(program):
+    i = 0
+    while True:
+        if run(program, [i, 0, 0]) == program:
+            return i
+        i += 1
 
-def assert_test_cases():
-    for (registers, program, expected_output, expected_registers) in test_cases:
-        output = part1(registers, program)
-        print(output, registers)
-        if expected_output: assert output == expected_output
-        if expected_registers: assert registers == expected_registers
 
-assert_test_cases()
-assert part1([729, 0, 0], [0, 1, 5, 4, 3, 0]) == '4,6,3,5,6,3,5,2,1,0'
-print(f'Part 1: {part1([23999685, 0, 0], [2, 4, 1, 1, 7, 5, 1, 5, 0, 3, 4, 4, 5, 5, 3, 0])}')
-# 2,4,1,5,1,6,5,5,0
-# 6,5,3,6,3,3,7,7,0
-# 4,4,4,3,0,6,6,2,1
+# assert part1([729, 0, 0], [0, 1, 5, 4, 3, 0]) == '4,6,3,5,6,3,5,2,1,0'
+# print(f'Part 1: {part1([23999685, 0, 0], [2, 4, 1, 1, 7, 5, 1, 5, 0, 3, 4, 4, 5, 5, 3, 0])}')
 
+# assert part2([0,3,5,4,3,0]) == 117440
+# print(f'Part 2: {part2([2, 4, 1, 1, 7, 5, 1, 5, 0, 3, 4, 4, 5, 5, 3, 0])}')
+# def prog(a):
+#     out, b, c = [], 0, 0
+#     while a != 0: # 3, 0
+#         b = a % 8  # 2, 4
+#         b = b ^ 1  # 1, 1
+#         c = a // 2 ** b  # 7, 5
+#         b = b ^ 5  # 1, 5
+#         a = a // 2 ** 3  # 0, 3
+#         b = b ^ c  # 4, 4
+#         out.append(b % 8)  # 5, 5
+#     print(out)
+#     return out
+
+# def prog(a):
+#     out, b, c = [], 0, 0
+#     while a != 0:
+#         b = a % 8 # 2, 4
+#         b = b ^ 1 # 1, 1
+#         #c = a // 2 ** b # 7, 5
+#         #b = b ^ 5 # 1, 5
+#         b = (b ^ 5) ^ (a // 2 ** b)  # b = b ^ c  (4, 4)
+#         a = a // 2 ** 3  # 0, 3
+#         out.append(b % 8) # 5, 5
+#     print(out)
+#     return out
+
+# def prog(a):
+#     out, b, c = [], 0, 0
+#     while a != 0:
+#         b = (a % 8) ^ 1 ^ 5 ^ (a // 2 ** ((a % 8) ^ 1))
+#         a = a // 8
+#         out.append(b % 8)
+#     print(out)
+#     return out
+
+def prog(a):
+    out, b, c = [], 0, 0
+    while a != 0:
+        out.append(((a % 8) ^ (a // 2 ** ((a % 8) ^ 1)) % 8) ^ 4)
+        a = a // 8
+    # print(out)
+    return out
+
+
+def output(a):
+    return ((a % 8) ^ (a // 2 ** ((a % 8) ^ 1)) % 8) ^ 4
+
+
+
+# for a in range(100):
+#     print(f'{a} {len(prog(a))}')
+# exit()
 #
-# assert part2('day17_input_test.txt') == 45
-# print(f'Part 2: {part2('day17_input.txt')}')
+
+
+target = [2, 4, 1, 1, 7, 5, 1, 5, 0, 3, 4, 4, 5, 5, 3, 0]
+
+def find_digits_before(index, sum):
+    if index == -1:
+        return sum
+    for a in range(sum * 8, (sum + 1) * 8):
+        # print(a)
+        out = output(a)
+        print(out)
+        # if digit == 15: print(out)
+        # print(out, target[len(target) - digit - 1])
+        if out == target[index]:
+            print(index)
+            print(a)
+            print(prog(a))
+            print()
+            found = find_digits_before(index - 1, a)
+            if found != -1:
+                return found
+        a += 1
+    return -1
+
+ans=find_digits_before(len(target) - 1, 0)
+print(ans)
+print(prog(ans))
+
+exit()
+# exit()
+
+# for i  in possible: print(prog(i))
+
+# print(prog(possible[-1]))
+# print(prog(possible[-2]))
+
+
+# (a % 8) ^ 1 ^ 5 ^ (a // 2 ** ((a % 8) ^ 1)) == n
+
+# assert prog(23999685) == [5, 0, 3, 5, 7, 6, 1, 5, 4]
+
+# print(f'Part 1: {part1([23999685, 0, 0], [2, 4, 1, 1, 7, 5, 1, 5, 0, 3, 4, 4, 5, 5, 3, 0])}')
+
+
+# print([2, 4, 1, 1, 7, 5, 1, 5, 0, 3, 4, 4, 5, 5, 3, 0])
+# for i in range(100):
+#     print(prog(10 ** 14 + i * 10 ** 2))
